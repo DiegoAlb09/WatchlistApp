a86f4a92b2657b9b117418077f3fbb17e9cf8a66
DiegoAlb09 <diegoalb04hat@gmail.com>
2026-07-10 16:16:50 -0600

    Agregar entidades WatchlistItemEntity y LibroItemEntity

---
+++ WatchlistApi/Models/WatchlistItemEntity.cs
@@ -0,0 +1,22 @@
+namespace WatchlistApi.Models;
+
+public enum TipoContenido
+{
+  Serie,
+  Pelicula
+}
+
+public class WatchlistItemEntity
+{
+  public Guid Id { get; set; }
+  public string Titulo { get; set; } = string.Empty;
+  public string PortadaUrl { get; set; } = string.Empty;
+  public TipoContenido Tipo { get; set; } = TipoContenido.Pelicula;
+  public int? EpisodioInicio { get; set; }
+  public int? EpisodioFin { get; set; }
+
+  public bool Visto { get; set; }
+
+  public List<int> EpisodiosVistos { get; set; } = new();
+  public DateTime FechaAgregado { get; set; } = DateTime.UtcNow;
+}
