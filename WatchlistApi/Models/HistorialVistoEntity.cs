0c18dd8439070c61fd1c4aa3f36352158cebbdc3
DiegoAlb09 <diegoalb04hat@gmail.com>
2026-07-19 18:28:44 -0600

    Agregar entidad HistorialVistoEntity

---
+++ WatchlistApi/Models/HistorialVistoEntity.cs
@@ -0,0 +1,9 @@
+namespace WatchlistApi.Models;
+
+public class HistorialVistoEntity
+{
+  public Guid Id { get; set; }
+  public Guid ItemId { get; set; }
+  public string Titulo { get; set; } = string.Empty;
+  public DateTime Fecha { get; set; } = DateTime.UtcNow;
+}
